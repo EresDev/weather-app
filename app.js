152ac89309276318221500ffe99748e1240eaaf6
EresDev <hi@eresdev.com>
2021-02-22 20:12:25 +0500

    feat: print avg temperature

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
 
-const url = 'https://api.aerisapi.com/forecasts/42.25,-95.25?'+
+const url = 'https://api.aerisapi.com/forecasts/40.741895,-73.98930777777778?'+
     'from=today&to=today'+
     '&client_id=' + argv.client_id +
     '&client_secret=' + argv.client_secret;
-request({url: url}, (error, response) => {
-    const jsonData = JSON.parse(response.body)
-    console.log(jsonData.response[0].periods)
+
+request({url: url, json: true}, (error, response) => {
+    const tempC = response.body.response[0].periods[0].avgTempC;
+    const feelsLike = response.body.response[0].periods[0].feelslikeC;
+
+    console.log('It is average '+tempC+' degree out. It feels like '+feelsLike+' degree out.')
 })
