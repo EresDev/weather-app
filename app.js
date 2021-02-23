3e7a1cccfd8d1084b63d51c6a1e5eba522d503b6
EresDev <hi@eresdev.com>
2021-02-23 18:57:12 +0500

    chore(code style): add statement terminator

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const request = require('request')
+const request = require('request');
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
 
 //API  Docs: https://openweathermap.org/current
-const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat=35&lon=139&appid='+argv.api_key
+const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat=35&lon=139&appid='+argv.api_key;
 
 request({url: url, json: true}, (error, response) => {
     const tempC = response.body.main.temp
     console.log('It is currently '+tempC+' degree out.')
-})
+});
