d8333489c18ed6801a2294cb7610e2e3b61fa20c
EresDev <hi@eresdev.com>
2021-03-10 20:11:25 +0500

    feat: accept location from cli

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const location = 'Los Angeles';
-
-geocode(location, (error, latlong) => {
-    if (error) {
-        console.log('Error', error);
-        return;
-    }
-
-    forecast(latlong.latitude, latlong.longitude, (error, data) => {
+const location = process.argv[6];
+if(!location){
+    console.error("Missing location in argument as the first argument.");
+} else{
+    geocode(location, (error, latlong) => {
         if (error) {
             console.log('Error', error);
             return;
         }
-        console.log(latlong.placename);
-        console.log(data);
+
+        forecast(latlong.latitude, latlong.longitude, (error, data) => {
+            if (error) {
+                console.log('Error', error);
+                return;
+            }
+            console.log(latlong.placename);
+            console.log(data);
+        });
     });
-});
+}
+
+
 
