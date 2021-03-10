bb1ddec46269b7a256a4962c24a9ab3e52fcfef4
EresDev <hi@eresdev.com>
2021-03-10 19:16:26 +0500

    refactor: extract forecast

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-const request = require('request');
-const geocode = require('./utils/geocode')
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
-
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const location = 'Los Angeles';
 
 geocode(location, (error, latlong) => {
     console.log('Error', error);
     console.log('Latlong', latlong);
-})
+});
 
-//API  Docs: https://openweathermap.org/current
-const latítude = '-73.9808';
-const longitude = '40.7648';
-const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat='+latítude+'&lon='+longitude+'&appid='+argv.api_key;
-
-request({url: url, json: true}, (error, response) => {
-    if (error) {
-        console.log(error);
-        console.log('Unable to connect to the weather API.');
-    } else if(response.body.cod == 400 ) {
-        console.log('Unable to find location for latlong.');
-    }else {
-        const tempC = response.body.main.temp;
-        console.log('It is currently '+tempC+' degree out.')
-    }
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 });
---
+++ utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+const apiKey = () => argv.api_key;
+//API  Docs: https://openweathermap.org/current
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lat='+latitude+'&lon='+longitude+'&appid='+apiKey();
+
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to the weather API.', undefined);
+        } else if(response.body.cod == 400 ) {
+            callback('Unable to find location for latlong.', undefined);
+        }else {
+            const tempC = response.body.main.temp;
+            callback(undefined, 'It is currently '+tempC+' degree out.');
+        }
+    });
+}
+
+module.exports = forecast;
